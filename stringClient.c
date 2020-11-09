0463c50bae9803b5cabdf041f8cb233ec5da1c25
zolaxiang <73020687+zolaxiang@users.noreply.github.com>
2020-11-09 10:06:28 -0500

    Add files via upload

---
+++ stringClient.c
@@ -0,0 +1,109 @@
+#include <errno.h>      
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+#define capacity 50
+#define max_length 10000
+
+char *a[capacity];
+int head=0, tail=0, num=0, end_program=0; //num: number of strings stored. end_program: when EOF is input.
+pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+
+void *input()
+{
+	int len;
+    while(1)
+    {
+		char temp[max_length];
+        if(fgets(temp, max_length, stdin)==NULL)
+		{
+			end_program=1; 
+			continue;
+		}
+		len=strlen(temp);
+		a[tail] = (char *) malloc(len+1);
+		strcpy(a[tail], temp);
+
+        tail=(tail+1)%capacity;
+        pthread_mutex_lock (&mtx);
+        num++;
+        pthread_mutex_unlock (&mtx);
+    }
+}
+
+int main()
+{
+	struct addrinfo hints, *res, *p;
+	int sockfd;
+
+	// first, load up address structs with getaddrinfo():
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_INET;  // use IPv4
+	hints.ai_socktype = SOCK_STREAM;
+
+	char *addr;
+	char *port;
+	addr = getenv ("SERVER_ADDRESS");
+	port = getenv ("SERVER_PORT");
+	getaddrinfo(addr, port, &hints, &res);
+
+	// make a socket and connect
+
+	for(p = res; p != NULL; p = p->ai_next) 
+	{
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+            continue;
+		if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) 
+		{
+            close(sockfd);
+            continue;
+        }
+        break;
+    }
+	
+	//
+	pthread_t thread;
+    pthread_create(&thread, NULL, input, NULL);
+	//
+	char buf[max_length];
+	while(1)
+	{		
+		if(end_program && num==0) //EOF has been input, and no unsent requests
+			exit(0);
+		if(num>0)
+        {
+			int i, len=strlen(a[head])+1;
+			char bytes[4];     //transform integer to four separate bytes.
+			for(i=0;i<4;i++)
+				bytes[i]= (len >> (8*i)) & 0xff;
+
+			send(sockfd, bytes, 4, 0);
+            send(sockfd, a[head], len, 0);
+
+			head=(head+1)%capacity;
+			pthread_mutex_lock (&mtx);
+            num--;
+            pthread_mutex_unlock (&mtx);
+
+			recv(sockfd, bytes, 4, 0);
+			int received=0;
+			while (received < len) //in case we need multiple recv()s.
+			{
+				int r = recv(sockfd, &buf[received], len-received, 0);
+				received += r;
+			}
+			printf("Server: %s", buf);
+            sleep(2);
+        }
+	}
+}
---
+++ stringServer.c
@@ -0,0 +1,124 @@
+#include <errno.h>      
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+
+void process(char *a) //transform to title-case
+{
+	int len=strlen(a);
+	int flag=1, i;
+	for(i=0; i< len; i++)
+	{
+		if(a[i]==9 || a[i]==32)
+			flag =1;
+		else
+		{
+			if(flag)
+			{
+				a[i]=toupper(a[i]);
+				flag=0;
+			}
+			else
+				a[i]=tolower(a[i]);
+		}
+	}
+}
+int main()
+{
+	fd_set master; 
+	fd_set read_fds; //file descriptor sets.
+	int fd_max;
+	int listener, new_fd;	
+
+	struct sockaddr_storage their_addr;
+	socklen_t addr_size;   //these two are for accepted sockets
+
+	struct addrinfo hints, *res;
+
+	// first, load up address structs with getaddrinfo():
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;  // use IPv4 or IPv6, whichever
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
+	getaddrinfo(NULL, "0", &hints, &res);
+	
+	//create socket and bind
+	listener = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	bind(listener, res->ai_addr, res->ai_addrlen);
+
+	//print server address and port
+	FILE *fp;
+	char name[101];
+	fp = popen("hostname -f", "r");
+	fgets(name, 100, fp);
+	printf("SERVER_ADDRESS %s", name);
+
+	struct sockaddr_in sin;
+	socklen_t len = sizeof(sin);
+	getsockname(listener, (struct sockaddr *)&sin, &len);
+	printf("SERVER_PORT %d\n", ntohs(sin.sin_port));
+		
+	//
+	FD_ZERO(&master);
+	FD_ZERO(&read_fds);
+	FD_SET(listener, &master);
+	fd_max = listener;
+	//
+	listen(listener, 10);
+//	printf("listener:%d\n", listener);
+	char buf[10000];
+	while(1)
+	{
+		read_fds = master;
+		select(fd_max+1, &read_fds, NULL, NULL, NULL);
+
+		int i;
+		for(i=0; i<=fd_max; i++)
+		{
+			if (FD_ISSET(i, &read_fds))
+			{
+				 if (i == listener) 
+				 {
+					addr_size = sizeof their_addr;
+					new_fd = accept(listener, (struct sockaddr *)&their_addr, &addr_size);
+					FD_SET(new_fd, &master);
+					if(new_fd> fd_max)
+						fd_max=new_fd;
+			//		printf("new_fd:%d\n", new_fd);
+				 }
+				 else
+				 {
+					char bytes[5];
+					if(recv(i, bytes, 4, 0) ==0)
+					{	
+						close(i);
+						FD_CLR(i, &master);
+			//			printf("closed\n");
+					}
+					else
+					{
+						int len= bytes[0]+ bytes[1]*256 + bytes[2]* 65536 + bytes[3]*16777216;
+			//			printf("len: %d\n", len);
+						int received=0;
+						while (received < len)
+						{
+							int r = recv(i, &buf[received], len-received, 0);
+							received += r;
+						}
+						printf("%s", buf);
+						process(buf);
+						send(i, bytes, 4, 0);
+						send(i, buf, len, 0);
+					}
+				 }
+			}
+		}
+	}
+}
